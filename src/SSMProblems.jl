4ab74e7452ec1452ed421a3d363664bdba75ee0f
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-06-13 21:19:45 +0100

    use recommended style for interface methods

--- src/SSMProblems.jl
+++ src/SSMProblems.jl
@@ -9,46 +9,32 @@ abstract type AbstractParticle end
 abstract type AbstractParticleCache end
 
 """
-    new_particle = transition!!(rng, step, particle, cache)
+    transition!!(rng, step, particle[, cache])
 
 Simulate the particle for the next time step from the forward dynamics.
 """
-function transition!!(
-    rng,
-    step,
-    particle::AbstractParticle,
-    cache::AbstractParticleCache = nothing,
-) end
+function transition!! end
 
 """
-    ℓM = transition_logdensity(step, particle, x, cache)
+    transition_logdensity(step, particle, x[, cache])
 
 (Optional) Computes the log-density of the forward transition if the density is available.
 """
-function transition_logdensity(
-    step,
-    particle::AbstractParticle,
-    x,
-    cache::AbstractParticleCache = nothing,
-) end
+function transition_logdensity end
 
 """
-    ℓπ = logdensity(step, particle, cache)
+    emission_logdensity(step, particle[, cache])
 
 Compute the log potential of current particle. This effectively "reweight" each particle.
 """
-function emission_logdensity(
-    step,
-    particle::AbstractParticle,
-    cache::AbstractParticleCache = nothing,
-) end
+function emission_logdensity end
 
 """
-    isdone(step, particle, cache=nothing)
+    isdone(step, particle[, cache])
 
 Determine whether we have reached the last time step of the Markov process. Return `true` if yes, otherwise return `false`.
 """
-function isdone(step, particle::AbstractParticle, cache::AbstractParticleCache = nothing) end
+function isdone end
 
 export transition!!, transition_logdensity, emission_logdensity, isdone, AbstractParticle
 
