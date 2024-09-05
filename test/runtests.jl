0f125a8419c517009efb2e2152e94d173ecbd143
THargreaves <tim.hargreaves@icloud.com>
2024-09-05 15:03:06 +0100

    Update unit tests to match aligned interface

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,26 +8,26 @@ using Test
         μ::Float64
         σ::Float64
     end
-    SSMProblems.distribution(dyn::TestLatentDynamics) = Normal(0, 1)
+    SSMProblems.distribution(dyn::TestLatentDynamics, extra) = Normal(0, 1)
     SSMProblems.distribution(dyn::TestLatentDynamics, step::Integer, prev_state, extra) =
         Normal(prev_state + dyn.μ, dyn.σ)
 
-    struct TestObservationProcess <: ObservationProcess{Float64,Float64}
+    struct TestObservationProcess <: ObservationProcess{Float64}
         σ::Float64
     end
     SSMProblems.distribution(obs::TestObservationProcess, step::Integer, state, extra) =
         Normal(state, obs.σ)
 
     model = StateSpaceModel(TestLatentDynamics(0.1, 0.2), TestObservationProcess(0.3))
-    println(eltype(model.dyn))
 
     rng = MersenneTwister(1234)
     T = 3
+    extra0 = nothing
     extras = [nothing for _ in 1:T]
 
     # Sampling with/without rng and extras
-    @test sample(rng, model, extras) isa Tuple
+    @test sample(rng, model, extra0, extras) isa Tuple
     @test sample(rng, model, T) isa Tuple
-    @test sample(model, extras) isa Tuple
+    @test sample(model, extra0, extras) isa Tuple
     @test sample(model, T) isa Tuple
 end
