a5a2a8dc9c997956e773676aa96b6a42423b3bc5
Tim Hargreaves <tim.hargreaves@icloud.com>
2024-09-09 15:29:59 +0100

    Included CUDA functions in benchmark

--- research/fast_gpu_ops/fast_cholesky.jl
+++ research/fast_gpu_ops/fast_cholesky.jl
@@ -59,6 +59,18 @@ function cpu_2d_analytical(As::Array{Float64,3})
     return out
 end
 
+function gpu_strided_lu(As::CuArray{Float32,3}; pivot::Bool=false)
+    # Note: not actually strided — just a helper wrapper
+    _, _, res = CUDA.CUBLAS.getrf_strided_batched(As, pivot)
+    return res
+end
+gpu_strided_lu_pivot(As::CuArray{Float32,3}) = gpu_strided_lu(As; pivot=true)
+
+function gpu_chol(As::Vector{CuArray{Float32,2,CUDA.DeviceMemory}})
+    CUDA.CUSOLVER.potrfBatched!('L', As)
+    return As
+end
+
 function gpu_2d_analytical(As::CuArray{Float32,3})
     out = CUDA.zeros(Float32, size(As))
     out[1, 1, :] .= sqrt.(As[1, 1, :])
@@ -68,9 +80,49 @@ function gpu_2d_analytical(As::CuArray{Float32,3})
     return out
 end
 
+benchmark_methods = Dict{Function,Function}(
+    cpu_native => random_psd_batch,
+    gpu_strided_lu => random_psd_cuda_strided_batch,
+    gpu_strided_lu_pivot => random_psd_cuda_strided_batch,
+    gpu_chol => random_psd_cuda_batch,
+)
+
+# 2D analytical methods
+if D == 2
+    push!(benchmark_methods, cpu_2d_analytical => random_psd_strided_batch)
+    push!(benchmark_methods, gpu_2d_analytical => random_psd_cuda_strided_batch)
+end
+
 ##################
 #### VALIDATE ####
 ##################
+
+A_test = random_psd(D)
+truth = cholesky(A_test).L
+truth_32 = convert.(Float32, truth)
+
+As = repeat(A_test, 1, 1, 1)
+res = cpu_2d_analytical(As)
+res = tril(res[:, :, 1])
+println("cpu_2d_analytical: ", res ≈ truth ? "passed" : "failed")
+
+As = cu(repeat(A_test, 1, 1, 1))
+res = gpu_2d_analytical(As)
+res = tril(Array(res[:, :, 1]))
+println("gpu_2d_analytical: ", res ≈ truth_32 ? "passed" : "failed")
+
+As = cu(repeat(A_test, 1, 1, 1))
+res = gpu_strided_lu(As; pivot=true)
+U = triu(Array(res[:, :, 1]))
+L = tril(Array(res[:, :, 1]))
+L[diagind(L)] .= 1.0
+A_recon = L * U
+println("gpu_lu: ", A_recon ≈ A_test ? "passed" : "failed")
+
+As = [cu(A_test)]
+res = gpu_chol(As)
+res = tril(Array(res[1]))
+println("gpu_chol: ", res ≈ truth_32 ? "passed" : "failed")
 
 ###################
 #### BENCHMARK ####
@@ -89,14 +141,6 @@ function benchmark(d, batch_sizes, methods)
     return results
 end
 
-benchmark_methods = Dict{Function,Function}(cpu_native => random_psd_batch)
-
-# 2D analytical methods
-if D == 2
-    push!(benchmark_methods, cpu_2d_analytical => random_psd_strided_batch)
-    push!(benchmark_methods, gpu_2d_analytical => random_psd_cuda_strided_batch)
-end
-
 # Run benchmark
 results = benchmark(D, BATCH_SIZES, benchmark_methods)
 
@@ -104,7 +148,18 @@ results = benchmark(D, BATCH_SIZES, benchmark_methods)
 #### VISUALISE ####
 ###################
 
-p = plot(; yscale=:log10, xaxis=:log10, xlabel="Batch size", ylabel="Time (s)")
+# Remove LU without pivoting
+delete!(results, "gpu_strided_lu")
+
+p = plot(;
+    yscale=:log10,
+    xaxis=:log10,
+    xlabel="Batch size",
+    ylabel="Time (s)",
+    legend=:topleft,
+    size=(800, 600),
+    title="Cholesky decomposition benchmark",
+)
 for (method, trials) in results
     plot!(p, BATCH_SIZES, median.(getfield.(trials, :times)) / 10^9; label=string(method))
 end
