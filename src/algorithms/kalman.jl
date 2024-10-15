54659d8fd57540c5db1c7a2d608e3be1bd38cef4
Charles Knipp <charles.a.knipp@frb.gov>
2024-10-15 12:22:36 -0400

    fixed RBPF marginalized states

--- src/algorithms/kalman.jl
+++ src/algorithms/kalman.jl
@@ -10,8 +10,7 @@ function initialise(
     rng::AbstractRNG, model::LinearGaussianStateSpaceModel, filter::KalmanFilter; kwargs...
 )
     μ0, Σ0 = calc_initial(model.dyn; kwargs...)
-    # TODO: preallocate innovations and Kalman gain
-    return GaussianContainer(Gaussian(μ0, Σ0), Gaussian(μ0, Σ0))
+    return Gaussian(μ0, Σ0)
 end
 
 function predict(
@@ -19,12 +18,12 @@ function predict(
     model::LinearGaussianStateSpaceModel,
     filter::KalmanFilter,
     step::Integer,
-    states::GaussianContainer;
+    states::Gaussian;
     kwargs...,
 )
-    μ, Σ = GaussianDistributions.pair(states.filtered)
+    μ, Σ = GaussianDistributions.pair(states)
     A, b, Q = calc_params(model.dyn, step; kwargs...)
-    states.proposed = Gaussian(A * μ + b, A * Σ * A' + Q)
+    states = Gaussian(A * μ + b, A * Σ * A' + Q)
     return states
 end
 
@@ -32,11 +31,11 @@ function update(
     model::LinearGaussianStateSpaceModel,
     filter::KalmanFilter,
     step::Integer,
-    states::GaussianContainer,
+    states::Gaussian,
     obs::AbstractVector;
     kwargs...,
 )
-    μ, Σ = GaussianDistributions.pair(states.proposed)
+    μ, Σ = GaussianDistributions.pair(states)
     H, c, R = calc_params(model.obs, step; kwargs...)
 
     # Update state
@@ -45,7 +44,7 @@ function update(
     S = H * Σ * H' + R
     K = Σ * H' / S
 
-    states.filtered = Gaussian(μ + K * y, Σ - K * H * Σ)
+    states = Gaussian(μ + K * y, Σ - K * H * Σ)
 
     # Compute log-likelihood
     ll = logpdf(MvNormal(m, S), obs)
