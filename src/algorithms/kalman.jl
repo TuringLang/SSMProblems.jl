639c0c704adf4fdb9a74c0e9f1bea3280f7e9047
Tim Hargreaves <tim.hargreaves@icloud.com>
2024-10-16 08:42:37 +0100

    Add fast analytical innovation inversion

--- src/algorithms/kalman.jl
+++ src/algorithms/kalman.jl
@@ -104,6 +104,58 @@ function predict(
     return (μs=μ̂s, Σs=Σ̂s)
 end
 
+function invert_innovation(S)
+    if size(S, 1) == 2
+        return _invert_innovation_analytic(S)
+    else
+        return _invert_innovation(S)
+    end
+end
+
+function _invert_innovation_analytic(S)
+    # Analytic Cholesky for 2x2 matrix
+    d_S = CUDA.zeros(Float32, size(S))
+    d_S[1, 1, :] .= sqrt.(S[1, 1, :])
+    d_S[2, 1, :] .= S[2, 1, :] ./ d_S[1, 1, :]
+    d_S[1, 2, :] .= d_S[2, 1, :]
+    d_S[2, 2, :] .= sqrt.(S[2, 2, :] .- d_S[2, 1, :] .^ 2)
+    # Analytic inverse of 2x2 matrix
+    S_inv = CUDA.zeros(Float32, size(S))
+    S_det = S[1, 1, :] .* S[2, 2, :] .- S[2, 1, :] .* S[1, 2, :]
+    S_inv[1, 1, :] .= S[2, 2, :]
+    S_inv[2, 2, :] .= S[1, 1, :]
+    S_inv[2, 1, :] .= -S[2, 1, :]
+    S_inv[1, 2, :] .= -S[1, 2, :]
+    S_inv ./= reshape(S_det, 1, 1, :)
+
+    diags = CuArray{Float32}(undef, size(S, 1), size(S, 3))
+    for i in 1:size(S, 1)
+        diags[i, :] .= d_S[i, i, :]
+    end
+    log_dets = 2 * sum(log ∘ abs, diags; dims=1)
+
+    return S_inv, log_dets
+end
+
+function _invert_innovation(S)
+    # LU decomposition to compute S^{-1}
+    # TODO: Replace with custom fast Cholesky kernel
+    d_ipiv, _, d_S = CUDA.CUBLAS.getrf_strided_batched(S, true)
+    S_inv = CuArray{Float32}(undef, size(S))
+    # TODO: This fails when D_obs > D_inner since S is not invertible
+    CUDA.CUBLAS.getri_strided_batched!(d_S, S_inv, d_ipiv)
+
+    diags = CuArray{Float32}(undef, size(S, 1), size(S, 3))
+    for i in 1:size(S, 1)
+        diags[i, :] .= d_S[i, i, :]
+    end
+    # L has ones on the diagonal so we can just multiply the diagonals of U
+    # Since we're using pivoting, diagonal entries may be negative, so we take the absolute value
+    log_dets = sum(log ∘ abs, diags; dims=1)
+
+    return S_inv, log_dets
+end
+
 function update(
     model::LinearGaussianStateSpaceModel{T},
     algo::BatchKalmanFilter,
@@ -121,12 +173,7 @@ function update(
 
     ΣH_T = NNlib.batched_mul(Σs, NNlib.batched_transpose(Hs))
 
-    # LU decomposition to compute S^{-1}
-    # TODO: Replace with custom fast Cholesky kernel
-    d_ipiv, _, d_S = CUDA.CUBLAS.getrf_strided_batched(S, true)
-    S_inv = CuArray{Float32}(undef, size(S))
-    # TODO: This fails when D_obs > D_inner since S is not invertible
-    CUDA.CUBLAS.getri_strided_batched!(d_S, S_inv, d_ipiv)
+    S_inv, log_dets = invert_innovation(S)
 
     K = NNlib.batched_mul(ΣH_T, S_inv)
 
@@ -134,14 +181,7 @@ function update(
     Σ_filt = Σs .- NNlib.batched_mul(K, NNlib.batched_mul(Hs, Σs))
 
     y = cu(obs)
-    # TODO: replace with custom kernel
-    diags = CuArray{Float32}(undef, size(S, 1), size(S, 3))
-    for i in 1:size(S, 1)
-        diags[i, :] .= d_S[i, i, :]
-    end
-    # L has ones on the diagonal so we can just multiply the diagonals of U
-    # Since we're using pivoting, diagonal entries may be negative, so we take the absolute value
-    log_dets = sum(log ∘ abs, diags; dims=1)
+
     inv_term = NNlib.batched_vec(S_inv, y .- m)
     log_likes =
         -0.5f0 * NNlib.batched_vec(reshape(y .- m, 1, size(y, 1), size(S, 3)), inv_term)
