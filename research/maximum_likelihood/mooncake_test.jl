56c6dc0817cf60192caa270be8cfa2c29e72b392
Charles Knipp <charles.a.knipp@frb.gov>
2025-03-13 10:49:01 -0400

    fixed Mooncake errors for Bootstrap filter

--- research/maximum_likelihood/mooncake_test.jl
+++ research/maximum_likelihood/mooncake_test.jl
@@ -32,32 +32,18 @@ using DifferentiationInterface
 import Mooncake
 using DistributionsAD
 
-function build_objective(rng, θ, algo, data)
+function build_objective(θ, algo, data)
+    rng = Xoshiro(1234)
     _, ll = GeneralisedFilters.filter(rng, toy_model(θ[]), algo, data)
     return -ll
 end
 
-# kalman filter likelihood testing (works, but is slow)
-logℓ1 = θ -> build_objective(rng, θ, KF(), ys)
+# kalman filter likelihood testing (is slow)
+logℓ1 = θ -> build_objective(θ, KF(), ys)
 Mooncake.TestUtils.test_rule(rng, logℓ1, [0.7]; is_primitive=false, debug_mode=true)
+DifferentiationInterface.gradient(logℓ1, AutoMooncake(; config=nothing), [0.7])
 
-# bootstrap filter likelihood testing (shouldn't work)
-logℓ2 = θ -> build_objective(rng, θ, BF(512), ys)
+# bootstrap filter likelihood testing (is even slower)
+logℓ2 = θ -> build_objective(θ, BF(512; threshold=0.1), ys)
 Mooncake.TestUtils.test_rule(rng, logℓ2, [0.7]; is_primitive=false, debug_mode=true)
-
-## FOR USE WITH DIFFERENTIATION INTERFACE ##################################################
-
-# data should be part of the objective, but be held constant by DifferentiationInterface
-logℓ3 = (θ, data) -> build_objective(rng, θ, KF(), data)
-
-# set the backend with default configuration
-backend = AutoMooncake(; config=nothing)
-
-# prepare the gradient for faster subsequent iteration
-grad_prep = prepare_gradient(logℓ3, backend, [0.7], Constant(ys))
-
-# evaluate gradients and iterate to show proof of concept
-DifferentiationInterface.gradient(logℓ3, grad_prep, backend, [0.7], Constant(ys))
-DifferentiationInterface.gradient(logℓ3, grad_prep, backend, [0.8], Constant(ys))
-DifferentiationInterface.gradient(logℓ3, grad_prep, backend, [0.9], Constant(ys))
-DifferentiationInterface.gradient(logℓ3, grad_prep, backend, [1.0], Constant(ys))
+DifferentiationInterface.gradient(logℓ2, AutoMooncake(; config=nothing), [0.7])
