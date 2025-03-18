6715cb68893d585449ff12ee395ec99e370733ac
Charles Knipp <charles.a.knipp@frb.gov>
2025-03-18 10:02:06 -0400

    add fix for Flux and Mooncake

--- research/variational_filter/script.jl
+++ research/variational_filter/script.jl
@@ -1,41 +1,37 @@
-using GeneralisedFilters
-using SSMProblems
-using PDMats
-using LinearAlgebra
-using Random
-using Distributions
-
-## LINEAR GAUSSIAN PROPOSAL ################################################################
-
-# this is a pseudo optimal proposal kernel for linear Gaussian models
-struct LinearGaussianProposal{T<:Real} <: GeneralisedFilters.AbstractProposal
-    φ::Vector{T}
-end
-
-# a lot of computations done at each step
-function GeneralisedFilters.distribution(
-    model::AbstractStateSpaceModel,
-    kernel::LinearGaussianProposal,
-    step::Integer,
-    state,
-    observation;
-    kwargs...,
-)
-    # get model dimensions
-    dx = length(state)
-    dy = length(observation)
-
-    # see (Corenflos et al, 2021) for details
-    A = GeneralisedFilters.calc_A(model.dyn, step; kwargs...)
-    Γ = diagm(dx, dy, kernel.φ[(dx + 1):end])
-    Σ = PDiagMat(kernel.φ[1:dx])
-
-    return MvNormal(inv(Σ) * A * state + inv(Σ) * Γ * observation, Σ)
-end
-
-## DEEP GAUSSIAN PROPOSAL ##################################################################
+using GeneralisedFilters, SSMProblems
+using PDMats, LinearAlgebra
+using Random, Distributions
 
 using Flux, Fluxperimental
+using DifferentiationInterface, Optimisers
+import Mooncake
+
+## TOY MODEL ###############################################################################
+
+# adapted from (Naesseth, 2016)
+function toy_model(::Type{T}, dx, dy) where {T<:Real}
+    A = begin
+        a = collect(1:dx)
+        @. convert(T, 0.42)^(abs(a - a') + 1)
+    end
+    b = zeros(T, dx)
+    Q = PDiagMat(ones(T, dx))
+
+    H = diagm(dy, dx, ones(T, dy))
+    c = zeros(T, dy)
+    R = PDiagMat(convert(T, 0.5)*ones(T, dy))
+
+    μ0 = zeros(T, dx)
+    Σ0 = PDiagMat(ones(T, dx))
+
+    return create_homogeneous_linear_gaussian_model(μ0, Σ0, A, b, Q, H, c, R)
+end
+
+rng = MersenneTwister(1234)
+true_model = toy_model(Float32, 10, 10)
+_, _, ys = sample(rng, true_model, 100)
+
+## DEEP GAUSSIAN PROPOSAL ##################################################################
 
 struct DeepGaussianProposal{T1,T2} <: GeneralisedFilters.AbstractProposal
     μ_net::T1
@@ -79,56 +75,35 @@ end
 
 ## VSMC ####################################################################################
 
-using DifferentiationInterface
-using Optimisers
-using DistributionsAD
-import ForwardDiff, Mooncake
-
-function toy_model(::Type{T}, dx, dy) where {T<:Real}
-    A = begin
-        a = collect(1:dx)
-        @. convert(T, 0.42)^(abs(a - a') + 1)
-    end
-    b = zeros(T, dx)
-    Q = PDiagMat(ones(T, dx))
-
-    H = diagm(dy, dx, ones(T, dy))
-    c = zeros(T, dy)
-    R = PDiagMat(convert(T, 0.5)*ones(T, dy))
-
-    μ0 = zeros(T, dx)
-    Σ0 = PDiagMat(ones(T, dx))
-
-    return create_homogeneous_linear_gaussian_model(μ0, Σ0, A, b, Q, H, c, R)
+# fix for Optimisers.update! with Flux support
+function Optimisers.update!(opt_state, params, grad::Mooncake.Tangent)
+    return Optimisers.update!(opt_state, params, Fluxperimental._moonstrip(grad))
 end
 
-rng = MersenneTwister(1234)
-true_model = toy_model(Float32, 10, 10)
-_, _, ys = sample(rng, true_model, 100)
-
-function logℓ(θ, data)
-    algo = GPF(4, LinearGaussianProposal(θ); threshold=1.0)
-    # algo = GPF(4, θ; threshold=1.0)
+function logℓ(φ, data)
+    algo = GPF(4, φ; threshold=0.8)
     _, ll = GeneralisedFilters.filter(true_model, algo, data)
     return -ll
 end
 
 num_epochs = 500
-θ = rand(rng, Float64, 20) .+ 1.0
-# θ = DeepGaussianProposal((10,10), (16,16))
-opt = Optimisers.setup(Adam(0.01), θ)
+φ = DeepGaussianProposal((10,10), (16,16))
+opt = Optimisers.setup(Adam(0.01), φ)
+vsmc_ll = zeros(Float32, num_epochs)
 
-backend = AutoForwardDiff()
+backend = AutoMooncake(;config=nothing)
 grad_prep = prepare_gradient(
-    logℓ, backend, θ, Constant(ys)
+    logℓ, backend, φ, Constant(ys)
 )
 
 @time for epoch in 1:num_epochs
     val, ∇logℓ = DifferentiationInterface.value_and_gradient(
-        logℓ, grad_prep, backend, θ, Constant(ys)
+        logℓ, grad_prep, backend, φ, Constant(ys)
     )
 
-    Optimisers.update!(opt, θ, ∇logℓ)
+    Optimisers.update!(opt, φ, ∇logℓ)
+    vsmc_ll[epoch] = val
+
     if (epoch % 25 == 0)
         println("\r$(epoch):\t -$(val)")
     else
