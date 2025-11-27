cab356f461c3beae3da570551180bf6430215c1b
Tim Hargreaves <tim.hargreaves@icloud.com>
2025-11-27 14:08:09 +0000

    Modify aqua tests to avoid type piracy false positive

--- GeneralisedFilters/test/Aqua.jl
+++ GeneralisedFilters/test/Aqua.jl
@@ -2,5 +2,14 @@ using Aqua
 using GeneralisedFilters
 
 @testset "Aqua.jl QA" begin
-    Aqua.test_all(GeneralisedFilters; deps_compat=false)
+    # Manually specify tests to allow controlling their behaviour
+    # Skip test_deps_compat
+    Aqua.test_ambiguities([GeneralisedFilters])
+    Aqua.test_unbound_args(GeneralisedFilters)
+    Aqua.test_undefined_exports(GeneralisedFilters)
+    Aqua.test_project_extras(GeneralisedFilters)
+    Aqua.test_stale_deps(GeneralisedFilters)
+    Aqua.test_piracies(GeneralisedFilters; treat_as_own=[SSMProblems.simulate_from_dist])
+    Aqua.test_persistent_tasks(GeneralisedFilters)
+    Aqua.test_undocumented_names(GeneralisedFilters)
 end
