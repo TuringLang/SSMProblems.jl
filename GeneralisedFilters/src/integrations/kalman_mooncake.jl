ebfb8fe71aa50f0de780187091eca39e60fe8ac3
Tim Hargreaves <tim.hargreaves@icloud.com>
2026-03-09 17:09:39 +0000

    Generalise Mooncake AD support to all AbstractPDMat

--- GeneralisedFilters/src/integrations/kalman_mooncake.jl
+++ GeneralisedFilters/src/integrations/kalman_mooncake.jl
@@ -11,54 +11,141 @@ The implementation properly handles both mutable (Vector, Matrix) and immutable
 """
 
 using Mooncake: Mooncake, @is_primitive, CoDual, primal, tangent
-using Mooncake: NoFData, NoRData, RData, Tangent
+using Mooncake: NoFData, NoRData, RData, Tangent, FData
 using Mooncake: zero_tangent, rdata, primal_to_tangent!!, increment!!
+using PDMats: PDiagMat
+
+## GRADIENT PROJECTION LAYER ###################################################################
+#
+# These functions project matrix gradients to parameter gradients. They are AD-framework
+# agnostic and could eventually move to PDMats.jl as ChainRulesCore.ProjectTo implementations.
 
 """
-    _update_vector_tangent!(fdata, idx, grad_value, primal_value)
+    _project_to_param(grad_matrix, primal)
+
+Project a gradient w.r.t. the full matrix representation to a gradient w.r.t. the
+parameter representation.
+
+For most types, this is identity. For structured covariance types like PDiagMat,
+this extracts the relevant components (e.g., diagonal).
+"""
+_project_to_param(grad, primal) = grad
+_project_to_param(grad::AbstractMatrix, ::PDMat) = grad
+_project_to_param(grad::AbstractMatrix, ::PDiagMat) = diag(grad)
+
+# TODO: Add ScalMat support if needed:
+# _project_to_param(grad::AbstractMatrix, ::ScalMat) = sum(diag(grad))
+
+## MOONCAKE TANGENT FIELD ACCESS ###############################################################
+#
+# These helpers encapsulate the tangent field structure for each PDMat type.
+# Dispatch is on primal type, keeping tangent structure knowledge in one place.
+
+"""
+    _get_param_value(primal)
+
+Return the underlying differentiable parameter for a PDMat type.
+"""
+_get_param_value(p::PDMat) = p.mat
+_get_param_value(p::PDiagMat) = p.diag
+
+"""
+    _param_tangent(tangent, primal)
+
+Extract the tangent for the differentiable parameter field from a PDMat tangent.
+"""
+_param_tangent(t::Tangent, ::PDMat) = t.fields.mat
+_param_tangent(t::Tangent, ::PDiagMat) = t.fields.diag
+_param_tangent(t::FData, ::PDMat) = t.data.mat
+_param_tangent(t::FData, ::PDiagMat) = t.data.diag
+
+"""
+    _with_param_tangent(old_tangent, new_param_tangent, primal)
+
+Reconstruct a PDMat tangent with an updated parameter field.
+"""
+function _with_param_tangent(t::Tangent, new_param, ::PDMat)
+    return Tangent((mat=new_param, chol=t.fields.chol))
+end
+function _with_param_tangent(t::Tangent, new_param, ::PDiagMat)
+    return Tangent((diag=new_param,))
+end
+
+## MOONCAKE TANGENT UPDATE #####################################################################
+
+"""
+    _update_vector_tangent!(fdata, idx, grad_matrix, primal_value)
 
 Accumulate the gradient into the tangent at index `idx` in the fdata vector.
-Uses `increment!!` to properly accumulate gradients, which is essential for
-handling aliased elements (e.g., from `fill(x, n)`).
+The gradient is first projected to parameter space, then converted to Mooncake
+tangent format and accumulated.
 """
 function _update_vector_tangent!(
-    fdata::AbstractVector, idx::Integer, grad_value, primal_value
+    fdata::AbstractVector, idx::Integer, grad_matrix, primal_value
 )
-    # Convert gradient to tangent type, then accumulate
-    grad_tangent = primal_to_tangent!!(zero_tangent(primal_value), grad_value)
+    grad_param = _project_to_param(grad_matrix, primal_value)
+    grad_tangent = primal_to_tangent!!(zero_tangent(primal_value), grad_param)
     fdata[idx] = increment!!(fdata[idx], grad_tangent)
     return nothing
 end
 
-# Special handling for PDMat: the gradient is w.r.t. the underlying matrix, not the PDMat itself.
-# The tangent for PDMat has structure Tangent{@NamedTuple{mat::..., chol::...}}.
-# We accumulate only into the mat field.
+# Specialization for AbstractPDMat: update only the parameter field
 function _update_vector_tangent!(
-    fdata::AbstractVector{<:Tangent{<:NamedTuple{(:mat, :chol)}}},
-    idx::Integer,
-    grad_value,
-    primal_value::PDMat,
+    fdata::AbstractVector, idx::Integer, grad_matrix, primal_value::AbstractPDMat
 )
+    grad_param = _project_to_param(grad_matrix, primal_value)
+    param_value = _get_param_value(primal_value)
+
     old_tangent = fdata[idx]
-    # Convert gradient to mat field tangent type, then accumulate
-    grad_mat_tangent = primal_to_tangent!!(zero_tangent(primal_value.mat), grad_value)
-    new_mat_tangent = increment!!(old_tangent.fields.mat, grad_mat_tangent)
-    # Reconstruct the PDMat tangent with accumulated mat field
-    fdata[idx] = Tangent((mat=new_mat_tangent, chol=old_tangent.fields.chol))
+    old_param_tangent = _param_tangent(old_tangent, primal_value)
+
+    grad_tangent = primal_to_tangent!!(zero_tangent(param_value), grad_param)
+    new_param_tangent = increment!!(old_param_tangent, grad_tangent)
+
+    fdata[idx] = _with_param_tangent(old_tangent, new_param_tangent, primal_value)
     return nothing
 end
 
-"""
-    _compute_rdata(grad_value, primal_value)
+## RDATA COMPUTATION ###########################################################################
 
-Compute the RData for an immutable input given its gradient value.
-Creates a zero tangent matching the primal, fills it with the gradient,
-and extracts the rdata portion.
 """
-function _compute_rdata(grad_value, primal_value)
-    t = primal_to_tangent!!(zero_tangent(primal_value), grad_value)
+    _compute_rdata(grad_matrix, primal_value)
+
+Compute the RData for an immutable input given its gradient (in matrix form).
+Projects to parameter space, creates a tangent, and extracts the rdata portion.
+"""
+function _compute_rdata(grad_matrix, primal_value)
+    grad_param = _project_to_param(grad_matrix, primal_value)
+    t = primal_to_tangent!!(zero_tangent(primal_value), grad_param)
     return rdata(t)
 end
+
+# For AbstractPDMat: build tangent with only the parameter field set
+function _compute_rdata(grad_matrix, primal_value::AbstractPDMat)
+    grad_param = _project_to_param(grad_matrix, primal_value)
+    param_value = _get_param_value(primal_value)
+
+    t_new = zero_tangent(primal_value)
+    t_param = primal_to_tangent!!(zero_tangent(param_value), grad_param)
+    new_tangent = _with_param_tangent(t_new, t_param, primal_value)
+    return rdata(new_tangent)
+end
+
+## FDATA UPDATE ################################################################################
+
+"""
+    _update_fdata!(fdata, grad_matrix, primal_value)
+
+Update mutable fdata in-place with the gradient. Used for standalone (non-vector) args.
+"""
+function _update_fdata!(fdata::FData, grad_matrix, primal_value::AbstractPDMat)
+    grad_param = _project_to_param(grad_matrix, primal_value)
+    param_fdata = _param_tangent(fdata, primal_value)
+    primal_to_tangent!!(param_fdata, grad_param)
+    return nothing
+end
+
+## MOONCAKE RRULE!! ############################################################################
 
 """
     Mooncake.rrule!!(::CoDual{typeof(kf_loglikelihood)}, ...)
@@ -68,7 +155,8 @@ The forward pass runs the KF with gradient caching; the pullback computes
 analytical gradients using the backward recursion from `kalman_gradient.jl`.
 
 Supports both mutable (Vector, Matrix, PDMat{Matrix}) and immutable
-(SVector, SMatrix, PDMat{SMatrix}) array types.
+(SVector, SMatrix, PDMat{SMatrix}) array types, as well as diagonal
+covariance types (PDiagMat).
 """
 function Mooncake.rrule!!(
     ::CoDual{typeof(kf_loglikelihood)},
@@ -128,7 +216,7 @@ function Mooncake.rrule!!(
             cache = caches[t]
             s = -Δll  # Convert from LL gradient to NLL gradient convention
 
-            # Observation parameter gradients
+            # Observation parameter gradients (as full matrices)
             grad_c = s * gradient_c(∂μ, cache)
             grad_H = s * gradient_H(∂μ, ∂Σ, cache, cache.Σ_pred, Hs_p[t])
             grad_R = s * gradient_R(∂μ, ∂Σ, cache)
@@ -166,16 +254,10 @@ function Mooncake.rrule!!(
         end
 
         # Handle Σ0: check if mutable or immutable
-        # Note: For PDMat, the gradient is w.r.t. the mat field
         rdata_Σ0 = if t_Σ0 isa NoFData
-            # Immutable PDMat (e.g., PDMat{SMatrix})
-            # Create a tangent and set the mat field
-            t_new = zero_tangent(Σ0_p)
-            t_mat = primal_to_tangent!!(t_new.fields.mat, grad_Σ0)
-            rdata(Tangent((mat=t_mat, chol=t_new.fields.chol)))
+            _compute_rdata(grad_Σ0, Σ0_p)
         else
-            # Mutable PDMat: update mat field in fdata
-            primal_to_tangent!!(t_Σ0.data.mat, grad_Σ0)
+            _update_fdata!(t_Σ0, grad_Σ0, Σ0_p)
             NoRData()
         end
 
